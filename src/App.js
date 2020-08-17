912e51f933c4ec8a35b6b470da3b5415a04865b7
Uchoosecode <awittyl44@gmail.com>
2020-08-17 08:09:02 -0400

    created checkIfCrashed function

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
 
   componentDidUpdate() {
     this.keepInArea();
+    this.checkIfCrashed();
   }
 
   onKeyDown = (e) => {
@@ -83,6 +84,18 @@ class App extends Component {
         this.onGameOver();
       }
   }
+
+  checkIfCrashed() {
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach(dot => {
+      if (head[0] == dot[0] && head[1] == dot[1]) {
+        this.onGameOver();
+      }
+    })
+  }
+
 
   onGameOver() {
     alert(`Game Over, Snake length is ${this.state.snakeDots.length}`);
