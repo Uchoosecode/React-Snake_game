44511528777e6734e5caeb7cd8a5cb91d0212ced
Uchoosecode <awittyl44@gmail.com>
2020-08-17 00:40:34 -0400

    created getRandomCoordinates function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import Snake from './Snake';
 import Food from './Food';
 
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y]
+}
 class App extends Component {
 
   state = {
-    food: [6,8],
+    food: getRandomCoordinates(),
+    direction: 'RIGHT',
     snakeDots: [
       [0,0],
       [2,0]
     ]
   }
+
+  componentDidMount() {
+    
+  }
+
   render() { 
     return (
       <div className="game-area">
